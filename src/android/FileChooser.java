799976de68c7b28ac65717ef23193d8a6d76ba42
Warren On Show <warrenjoe92@gmail.com>
2016-02-04 12:19:22 +1100

    modified plugin to get file name

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import android.provider.OpenableColumns;
+import android.database.Cursor;
+import android.content.Context;
 
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FileChooser extends CordovaPlugin {
 
@@ -58,13 +62,18 @@ public class FileChooser extends CordovaPlugin {
 
                 if (uri != null) {
 
-                    Log.w(TAG, uri.toString());
-                    callback.success(uri.toString());
+                  try{
+                    String displayName = getFileName(uri);
+                    JSONObject json = new JSONObject();
+                    json.put("uri", uri.toString());
+                    json.put("filename", displayName);
+                    callback.success(json);
+                  }catch(JSONException e){
+                    callback.error("JSON error");
+                  }
 
                 } else {
-
                     callback.error("File uri was null");
-
                 }
 
             } else if (resultCode == Activity.RESULT_CANCELED) {
@@ -78,5 +87,42 @@ public class FileChooser extends CordovaPlugin {
                 callback.error(resultCode);
             }
         }
+    }
+
+    private String getFileName(Uri uri){
+      Context c = this.cordova.getActivity().getApplicationContext();
+      Cursor cursor = c.getContentResolver().query(uri, null, null, null, null, null);
+      try {
+      // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
+      // "if there's anything to look at, look at it" conditionals.
+          if (cursor != null && cursor.moveToFirst()) {
+
+              // Note it's called "Display Name".  This is
+              // provider-specific, and might not necessarily be the file name.
+              String displayName = cursor.getString(
+                      cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+
+              // int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+              // If the size is unknown, the value stored is null.  But since an
+              // int can't be null in Java, the behavior is implementation-specific,
+              // which is just a fancy term for "unpredictable".  So as
+              // a rule, check if it's null before assigning to an int.  This will
+              // happen often:  The storage API allows for remote files, whose
+              // size might not be locally known.
+              // String size = null;
+              // if (!cursor.isNull(sizeIndex)) {
+              //     // Technically the column stores an int, but cursor.getString()
+              //     // will do the conversion automatically.
+              //     size = cursor.getString(sizeIndex);
+              // } else {
+              //     size = "Unknown";
+              // }
+
+              return displayName;
+          }
+      } finally {
+          cursor.close();
+      }
+      return "";
     }
 }
