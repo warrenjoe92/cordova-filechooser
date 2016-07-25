0b2e9c932f760b079bb0746b0ba77576ba9b84d5
Warren On Show <warrenjoe92@gmail.com>
2016-07-25 16:43:02 +1000

    removed file name fetching FN

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
-import android.provider.OpenableColumns;
-import android.database.Cursor;
 import android.content.Context;
 
 import org.apache.cordova.CordovaArgs;
@@ -61,34 +59,22 @@ public class FileChooser extends CordovaPlugin {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-      Log.v(TAG, "on activity result");
-
         if (requestCode == PICK_FILE_REQUEST && callback != null) {
-
-          Log.v(TAG, "pick file request");
 
             if (resultCode == Activity.RESULT_OK) {
 
-              Log.v(TAG, "activity result ok");
-
                 Uri uri = data.getData();
-
-                Log.v(TAG, "got data");
 
                 if (uri != null) {
 
                   try{
 
-                    Log.v(TAG, "uri not null, getting info");
-
-                    String displayName = getFileName(uri);
                     String mimeType = getMimeType(uri);
                     JSONObject json = new JSONObject();
                     json.put("uri", uri.toString());
-                    json.put("filename", displayName);
                     json.put("mimeType", mimeType);
 
-                    Log.v(TAG, "got all info: " + displayName + " " + mimeType);
+                    Log.v(TAG, "got all info: " + " mime " + mimeType);
                     Log.v(TAG, "executing callback");
 
                     callback.success(json);
@@ -122,53 +108,9 @@ public class FileChooser extends CordovaPlugin {
       Log.v(TAG, "get mime type");
 
       Context c = this.cordova.getActivity().getApplicationContext();
+
       return c.getContentResolver().getType(uri);
+
     }
 
-    private String getFileName(Uri uri){
-
-      Log.v(TAG, "get file name");
-
-      Context c = this.cordova.getActivity().getApplicationContext();
-      Cursor cursor = c.getContentResolver().query(uri, null, null, null, null, null);
-      try {
-      // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
-      // "if there's anything to look at, look at it" conditionals.
-          if (cursor != null && cursor.moveToFirst()) {
-
-              // Note it's called "Display Name".  This is
-              // provider-specific, and might not necessarily be the file name.
-              String displayName = cursor.getString(
-                      cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)); //causes NULL_POINTER exception on some devices
-
-              // int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
-              // If the size is unknown, the value stored is null.  But since an
-              // int can't be null in Java, the behavior is implementation-specific,
-              // which is just a fancy term for "unpredictable".  So as
-              // a rule, check if it's null before assigning to an int.  This will
-              // happen often:  The storage API allows for remote files, whose
-              // size might not be locally known.
-              // String size = null;
-              // if (!cursor.isNull(sizeIndex)) {
-              //     // Technically the column stores an int, but cursor.getString()
-              //     // will do the conversion automatically.
-              //     size = cursor.getString(sizeIndex);
-              // } else {
-              //     size = "Unknown";
-              // }
-
-              return displayName;
-          }
-      } catch (NullPointerException exc) {
-          Log.w(TAG, "null pointer exception");
-      }finally {
-          try{
-              cursor.close();
-          }
-          catch (NullPointerException exc) {
-              Log.w(TAG, "null pointer exception");
-          }
-      }
-      return null;
-    }
 }
